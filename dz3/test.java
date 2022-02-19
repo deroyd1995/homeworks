0d16860f018621d279306a829ad3652575ee4b99
koshl <koshlakov.deroyd@mail.ru>
2022-02-19 19:08:39 +0300

    modified searchinh palindrome alhorythm

--- dz3/test.java
+++ dz3/test.java
@@ -8,7 +8,7 @@ public class test {
     public static void main(String[] args) {
 
         //основная строка
-        String str = "оооррооо";
+        String str = "eqnтптототптпоакиремас";
 
         StringBuilder plain = new StringBuilder(str);
         StringBuilder stra = plain.reverse();
@@ -19,6 +19,7 @@ public class test {
         char[] charArray = str.toCharArray();
         char[] charArray2 = str2.toCharArray();
         char[] result = new char[charArray.length];
+        String[] strresult = new String[charArray.length];
 
 
         //charArray[i]  0:length-1 (1:length-1)
@@ -30,12 +31,14 @@ public class test {
         boolean found = false; //если нашли элемент не на той же позиции в строке, где перебираемый
         int p = 0;
 
-
+        here:
         for (int i = charArray2.length -1; i >=0 ; i--) {
             if (found == true){
                 break;
             }
             int fPos = 0; //найденная позиция элемента
+
+
             for (int j = p; j <= charArray.length-1; j++){
                 if ((charArray2[i] == charArray[j])&&(found == false)) {
                     if (charArray.length - 1 - j != i) {
@@ -43,15 +46,21 @@ public class test {
                         //если мы до этого нашли элемент где-то в строке, но не на той же зеркальной позиции
                         int p1 = charArray.length - 1 - i;
                         for (int i1 = charArray2.length - 1 - fPos; i1 <= charArray2.length - 1; i1++) {
-                            for (int j1 = p; j1 <= fPos; j1++) {
+                            for (int j1 = p1; j1 <= fPos; j1++) {
                                 if (charArray2[i1] == charArray[j1]) {
                                     p1 = p1 + 1;
                                     result[i1] = charArray2[i1];
                                     break;
                                 }
+                                else{
+                                    Arrays.fill(result, '\u0000');
+                                    found = true;
+                                    continue here;
+                                }
                             }
-                        }
 
+                        }
+                        strresult[i] = String.valueOf(result);
                         found = true;
                         continue;
                     }
@@ -67,15 +76,17 @@ public class test {
 
 
 
-        //result[j]=charArray[j];
-        //Arrays.deepToString(result);
-        if (result[6]=='\u0000'){
-            System.out.println("asd");
-        }
 
         //result = result.replace("\u0000", "");
-        String strresult = String.valueOf(result);
-        System.out.println(strresult.replace("\u0000", ""));
+        //String strresult1 = String.valueOf(strresult[7]);
+        //System.out.println(strresult1.replace("\u0000", ""));
+
+        if (String.valueOf(result).replace("\u0000", "") == ""){
+            System.out.println("Палиндрома не найдено");
+        }
+        else{
+            System.out.println(String.valueOf(result).replace("\u0000", ""));
+        }
 
     }
 }
