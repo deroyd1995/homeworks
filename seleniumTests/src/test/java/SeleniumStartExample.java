e6a88b526ce2a14c8fbb37b196ab56ceb324bc65
koshl <koshlakov.deroyd@mail.ru>
2022-03-26 20:08:00 +0300

    finalized weather tests added cropped schreenshot

---
+++ seleniumTests/src/test/java/SeleniumStartExample.java
@@ -0,0 +1,125 @@
+import io.netty.handler.codec.http.multipart.DiskFileUpload;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxDriverLogLevel;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.TimeoutException;
+
+import static org.openqa.selenium.io.FileHandler.copy;
+
+public class SeleniumStartExample {
+    private WebDriver webDriver;
+
+    private static final String baseUrl = "https://yandex.ru/";
+
+    //локаторы
+    private static final String inputField = "//input[@aria-label='Запрос']";
+    private static final String YANDEX_LOGO = "//div[@aria-label='Яндекс']";
+    private static final String find_button = "//span[text()='Найти']/parent::button";
+    private static final String resultList = "//ul[@aria-label='Результаты поиска']";
+    private static final String WEATHER = "//div[@class='weather__header']//a[text()='Погода']";
+    private static final String currentTemperature = "//div[@class='weather__temp']";
+    private static final String tomorrowTemperature = "//div[text()='Сегодня']/ancestor::div[1]/following-sibling::div[1]//span[text()='днём']/following-sibling::span[contains(@class,'temp__value')]";
+    private static final String tomorrowTemperatureCropp = "//div[text()='Сегодня']/ancestor::div[1]/following-sibling::div[1]";
+    private static final String closeAd = "//div[@class=\"modal__close\"]";
+
+    @BeforeEach
+    public void setUp() throws java.util.concurrent.TimeoutException {
+        System.setProperty("webdriver.gecko.driver","src\\test\\resources\\geckodriver.exe");
+        System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE,"/dev/null");
+        webDriver = new FirefoxDriver();
+        System.out.println("Driver was started");
+        webDriver.get(baseUrl);
+        webDriver.manage().window().maximize();
+        try{
+            WebElement closeAdButton = new WebDriverWait(webDriver, Duration.ofSeconds(5))
+                    .until(ExpectedConditions.elementToBeClickable(By.xpath(closeAd)));
+            webDriver.findElement(By.xpath(closeAd)).click();
+        }
+        catch(TimeoutException e){};
+    }
+
+    @Test
+    public void freeTest(){
+        System.out.println("success");
+    }
+
+
+    @Test
+    @DisplayName("Find the best motivators")
+    public void findBestMotivatorTest() throws InterruptedException{
+        Assertions.assertTrue(webDriver.findElement(By.xpath(YANDEX_LOGO)).isDisplayed());
+        webDriver.findElement(By.xpath(inputField)).sendKeys("лучшие мотиваторы");
+        webDriver.findElement(By.xpath(find_button)).click();
+        WebElement firstResult = new WebDriverWait(webDriver, Duration.ofSeconds(18))
+        .until(ExpectedConditions.elementToBeClickable(By.xpath(resultList)));
+        System.out.println("Motivators found successfully");
+    }
+
+    @Test
+    @DisplayName("Get today temperature")
+    public void findCurrentTemperatureTest(){
+        String todayTemperature;
+        todayTemperature = webDriver.findElement(By.xpath(currentTemperature)).getText();
+        System.out.println("Today is "+todayTemperature);
+    }
+
+    @Test
+    @DisplayName("Get tomorrow temperature")
+    public void findTomorrowTemperatureTest() throws InterruptedException, IOException{
+        //String oldTab = webDriver.getWindowsHandle();
+        String TomorrowTemperature;
+        WebElement webElement = webDriver.findElement(By.xpath(WEATHER));
+        webElement.click();
+        TimeUnit.SECONDS.sleep(5);
+        //ArrayList<String> newTab - new ArrayList<String>(webDriver.getWindowHandles());
+        //newTab.remove(oldTab);
+        //webDriver.switchTo().window(newTab.get(0));
+
+        for (String windowHandle : webDriver.getWindowHandles()){
+            webDriver.switchTo().window(windowHandle);
+        }
+        TomorrowTemperature = webDriver.findElement(By.xpath(tomorrowTemperature)).getText();
+        System.out.println("Tomorrow is "+TomorrowTemperature);
+        File scrFile = ((TakesScreenshot)webDriver).getScreenshotAs(OutputType.FILE);
+        copy(scrFile , new File("src\\test\\resources\\screenshot.png"));
+
+
+        //reading image
+        BufferedImage  fullImg = ImageIO.read(scrFile);
+
+        // Get the location of element on the page
+        webElement = webDriver.findElement(By.xpath(tomorrowTemperatureCropp));
+        Point point = webElement.getLocation();
+
+        // Get width and height of the element
+        int eleWidth = webElement.getSize().getWidth();
+        int eleHeight = webElement.getSize().getHeight();
+
+        // Crop the entire page screenshot to get only element screenshot
+        BufferedImage eleScreenshot= fullImg.getSubimage(point.getX(), point.getY(),
+                eleWidth, eleHeight);
+        ImageIO.write(eleScreenshot, "png", scrFile);
+        File screenshotLocation = new File("src\\test\\resources\\screenshot_cropped.png");
+        copy(scrFile, screenshotLocation);
+
+    }
+
+    @AfterEach
+    public void tearDown(){
+        System.out.println("Driver was shutdown successfuly");
+        webDriver.quit();
+    }
+}
